07fdf55c46010bae74cec92c09a13dbf76e0d3bf
henrry-sakurazaka <bright.lights.21@me.com>
2024-08-28 17:36:47 +0900

    firebaseInit.jsまでコピー

---
+++ src/firebaseInit.js
@@ -0,0 +1,32 @@
+
+// tests/testEntry.js
+
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+
+// Firebaseの設定
+const firebaseConfig = {
+  // your firebase config here
+apiKey: "AIzaSyCFn-eJuAP2f2zYP4VxMvvwef15jzyW7bA",
+authDomain: "reminder3-65e84.firebaseapp.com",
+databaseURL: "https://reminder3-65e84-default-rtdb.firebaseio.com",
+projectId: "reminder3-65e84",
+storageBucket: "reminder3-65e84.appspot.com",
+messagingSenderId: "280162142902",
+appId: "1:280162142902:web:4fed1bc9d4b35e75963417",
+measurementId: "G-C0NL3GWNWZ"
+};
+
+// Firebaseアプリの初期化
+const app = initializeApp(firebaseConfig);
+
+// Firebaseの各機能を取得
+const auth = getAuth(app);
+const db = getDatabase(app);
+
+// // Firebase関連の初期化を行うファイルをインポート
+// import '../src/firebaseInit';
+
+// // テストを行うファイルをインポート
+// import '../src/components/FirstAuth.test';
