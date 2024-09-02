82f6862248dfd35c1781fa79befb7d8a8d01e516
henrry-sakurazaka <bright.lights.21@me.com>
2024-09-02 22:26:22 +0900

    Reminderアプリ、とりあえず完成

---
+++ src/components/Example2.js
@@ -0,0 +1,58 @@
+import React from "react";
+import Todo from "./Todo";
+import "./Todo.css" ;
+import { useTodos } from "../context/TodoContext";
+
+
+const Example = () => {
+
+  
+    const { modalOpen, timeCheck } = useTodos();
+    
+    return (
+        <>
+         <div className="triangle"></div>
+            <div className="decoration">
+                <span className="slash"></span>
+                <span className="slash"></span>
+                <span className="slash"></span>
+                <span className="slash"></span>
+                <span className="slash"></span>
+            </div>
+      
+        <div className="container">
+            <section>
+                <div className="big-title">
+                    {/* <h1 className="big-text" style={{color: modalOpen && " rgba(40, 147, 247, 0.772)" , color: timeCheck && modalOpen && "rgb(8, 232, 158)"}}  >REMINDER</h1>
+                     */}
+                     <h1 className="big-text" 
+                        style={{ 
+                            color: timeCheck && modalOpen ? "rgb(8, 232, 158)" 
+                            : modalOpen ? "rgba(40, 147, 247, 0.772)":  "rgb(8, 232, 158)"  
+                        }} >
+                        REMINDER
+                    </h1>
+
+                </div>
+                    <div className="clone-big-title"><h1 className="clone-text">REMINDER</h1></div>
+                    <div className="discription">
+                        <p>Manage your tasks and make your lifestyle meaningful 
+                        with the Reminder App. Let's get started. You can input 
+                        your requirements and add them by pressing the plus
+                        button. Once your tasks are successfully executed and completed, 
+                        press the complete button. You can delete a task by double-clicking it.
+                        Once all tasks are completed, you can clear them using the refresh button.
+                        Be careful, as pressing the refresh button is irreversible.
+                        Have a great lifestyle!</p>
+                    </div>
+                <div className="out-line">
+                    <h2 className="title">Reminder</h2> 
+                        <Todo/>         
+                </div>
+           </section>     
+        </div>    
+        </>
+    );
+}
+
+export default Example;
