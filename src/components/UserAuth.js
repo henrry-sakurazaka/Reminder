3f0182f76b08108fdcb2b7211a561ef3191a6ae6
henrry-sakurazaka <bright.lights.21@me.com>
2024-09-07 23:54:32 +0900

    認証機能を強化しました。

--- src/components/UserAuth.js
+++ src/components/UserAuth.js
@@ -9,6 +9,14 @@ const auth = getAuth();
 const UserAuth = () => {   
     const navigate = useNavigate();
 
+    const spans = [1, 2, 3, 4, 5]; // spanの数だけ適当な配列を作成fi
+
+    // const getColor = () => "rgb(8, 232, 158)";
+    const getColor = () => "rgba(40, 147, 247, 0.772)";
+    const getColor2 = () => "yellowGreen";
+
+  
+
     const handleClickSignUp = () => {
         navigate('/SignUp');
     }
@@ -32,17 +40,36 @@ const UserAuth = () => {
         navigate('/EasyLogin');
     }
 
+    const handleDeleteAccount = () => {
+        navigate('/DeleteAccount');
+    }
+
     return (
-            
-                <div className="container">
+        <> 
+            <div className="container">
+            <div className="decoration-container">
+                <div className="decoration2">
+                    {spans.map((_, index) => (
+                    <span 
+                        key={index} 
+                        className="slash2" 
+                        style={{ backgroundColor: getColor2() }}
+                    ></span>
+                    ))}
+                </div>
+            </div>
+                <div className="circle2">
                     <div className="inner9-container">
                         <span className="select-auth" onClick={() => handleClickSignUp()}>Sign Up</span>
                         <span className="select-auth" onClick={() => handleClickSignIn()}>Sign In</span>
                         <span className="select-auth" onClick={() => handleClickSignOut()}>Sign Out</span>
                         <span className="select-auth" onClick={() => handleClickEasyLogin()}>お試しログイン</span>
+                        <span className="select-auth" onClick={() => handleDeleteAccount()}>Delete Account</span>
                     </div>
-                </div>
-
+                </div> 
+            </div>
+        </>
+        
     )
 }
 
