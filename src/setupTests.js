a505848c50d944f09c08d39c8260d4944e5169aa
henrry-sakurazaka <bright.lights.21@me.com>
2024-09-16 11:27:57 +0900

    jest.setup.jsを削除、src/setupTest.jsを採用

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,8 +2,10 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// import '@testing-library/jest-dom';
 import { TextEncoder, TextDecoder } from 'text-encoding';
+import '@testing-library/jest-dom/extend-expect';
+
 
 // Polyfill for encoding which isn't present globally in jsdom
 if (typeof global.TextEncoder === 'undefined') {
