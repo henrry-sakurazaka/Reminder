195e83e9af5027831144c25ad0afc923e517e6ba
henrry-sakurazaka <bright.lights.21@me.com>
2024-09-19 18:50:08 +0900

    E2E tests workflow again

--- tests/3_edit.spec.js
+++ tests/3_edit.spec.js
@@ -6,20 +6,20 @@ test.use({
     channel: 'chrome' // PlaywrightでChromeを使用するように指定
   });
 
-  test.setTimeout(10000);
+  test.setTimeout(1200000);
 
   test('タスクをフォームを入力して出力を確認', async ({ page }) => {
      
       await page.goto('https://reminder3-65e84.web.app/Example');
-      await page.waitForTimeout(10000);
+      await page.waitForTimeout(40000);
       await page.fill('input#task','test');
       const lastSpan = page.locator('span').last(); 
-      await lastSpan.waitFor({timeout: 20000});
+      await lastSpan.waitFor({timeout: 40000});
       await page.click('button.add');
-      await page.waitForSelector(lastSpan, {timeout: 20000});
+      await page.waitForSelector(lastSpan, {timeout: 40000});
       await expect(lastSpan).toHaveText('test');
       const lastBtn = page.locator('button.compBtn').last();
-      await lastBtn.waitFor({timeout: 20000});
+      await lastBtn.waitFor({timeout: 40000});
       await lastBtn.click();
       await expect(page.locator(lastBtn)).toHaveText('Completed');
       await expect(page.locator(lastBtn)).toHaveCSS('color', 'rgb(8, 232, 158)');
