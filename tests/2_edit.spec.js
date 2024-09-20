67525c0c0dfe328f91200aad4f17a1c9afed2e6c
henrry-sakurazaka <bright.lights.21@me.com>
2024-09-20 15:38:04 +0900

    E2E tests workflow again

--- tests/2_edit.spec.js
+++ tests/2_edit.spec.js
@@ -12,8 +12,8 @@ test.use({
 
       await page.goto('https://reminder3-65e84.web.app/SignIn');  
         
-      const email = process.env.REACT_APP_TEST2_EMAIL
-      const password = process.env.REACT_APP_TEST2_PASSWORD; 
+      const email = process.env.REACT_APP_TEST_EMAIL
+      const password = process.env.REACT_APP_TEST_PASSWORD; 
 
       await page.fill('#email', email); 
       await page.fill('#password', password); 
