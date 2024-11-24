5113626a8b160d832223cfea0efd0c71562626cf
henrry-sakurazaka <bright.lights.21@me.com>
2024-11-24 20:00:40 +0900

    try

--- playwright.config.js
+++ playwright.config.js
@@ -5,6 +5,10 @@ export default defineConfig({
   testDir: './tests',  // テストファイルのディレクトリ
   timeout: 30000,  // テストのタイムアウト時間
   retries: 1,  // テストのリトライ回数
+  reporter: [
+    ['json', { outputFile: 'playwright-report/output.json' }],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+  ],
   use: {
     headless: true,  // ヘッドレスモードで実行（表示なし）
     viewport: { width: 1280, height: 720 },  // ビューポートの設定
