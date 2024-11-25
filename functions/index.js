605405831cb74b381afd3122f248a83521345afd
henrry-sakurazaka <bright.lights.21@me.com>
2024-11-25 23:34:37 +0900

    docker.compose.ymlを修正

--- functions/index.js
+++ functions/index.js
@@ -22,7 +22,7 @@ const app = express();
 
 // CORSのミドルウェアを設定
 const corsOptions = {
-    origin: ['https://reminder3-65e84.web.app', 'https://localhost:443'],
+    origin: ['https://reminder3-65e84.web.app', 'https://localhost'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
     credentials: true,
