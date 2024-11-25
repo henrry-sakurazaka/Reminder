37c0d753641d23c680913506765bb99f35a21d6b
henrry-sakurazaka <bright.lights.21@me.com>
2024-11-25 21:53:53 +0900

    nginx.confを修正

--- tests/2_modal.spec.js
+++ tests/2_modal.spec.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 //   });
 
   test('タスクをフォームを入力して出力を確認', async ({ page }) => {
-    const baseUrl = process.env.VITE_REACT_APP_API_URL
+    const baseUrl = process.env.VITE_REACT_APP_API_URL || 'https://localhost';
     const email = process.env.VITE_REACT_APP_TEST_EMAIL
     const password = process.env.VITE_REACT_APP_TEST_PASSWORD; 
    
