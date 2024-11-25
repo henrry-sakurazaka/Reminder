24c6e2edfee001af21439c4319299cbb3dbc75bd
henrry-sakurazaka <bright.lights.21@me.com>
2024-11-25 23:01:43 +0900

    testコードのdotenvを条件分岐でci環境と区別

--- tests/2_modal.spec.js
+++ tests/2_modal.spec.js
@@ -1,5 +1,9 @@
 const { test, expect } = require('@playwright/test');
-require('dotenv').config();
+
+// 環境がローカルであれば.envを読み込む
+if (process.env.CI !== 'true') {
+  require('dotenv').config();
+}
 
 // test.use({
 //     browserName: 'chromium',
