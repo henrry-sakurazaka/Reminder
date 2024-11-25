9071b9ff68dcaa35f3342bc59749b3822045d2fd
henrry-sakurazaka <bright.lights.21@me.com>
2024-11-25 09:29:22 +0900

    .envの読み込みを修正

--- tests/3_edit.spec.js
+++ tests/3_edit.spec.js
@@ -10,17 +10,18 @@ require('dotenv').config();
   test.setTimeout(1200000);
 
   test('タスクをフォームを入力して出力を確認', async ({ page }) => {
-      await page.waitForTimeout(5000);   
-      await page.goto(`${process.env.VITE_REACT_APP_API_URL}/SignIn`);  
-        
+      const baseUrl =process.env.VITE_REACT_APP_API_URL
       const email = process.env.VITE_REACT_APP_TEST_EMAIL
       const password = process.env.VITE_REACT_APP_TEST_PASSWORD; 
       
+      await page.waitForTimeout(5000);   
+      await page.goto(`${baseUrl}/SignIn`);  
+        
       await page.fill('#email', email); 
       await page.fill('#password', password); 
       await page.click('button.form-button[type="submit"]');
     
-     await expect(page).toHaveURL(`${process.env.VITE_REACT_APP_API_URL}/Example`);
+      await expect(page).toHaveURL(`${baseUrl}/Example`);
         
         
       await page.waitForTimeout(40000);
