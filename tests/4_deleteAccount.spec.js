2452af410dc4add7fd78b425bfe3829d341ddb8d
henrry-sakurazaka <bright.lights.21@me.com>
2024-11-26 09:46:29 +0900

    修正

--- tests/4_deleteAccount.spec.js
+++ tests/4_deleteAccount.spec.js
@@ -14,7 +14,7 @@ if (process.env.CI !== 'true') {
             body: JSON.stringify({ success: true }),
         });
     });
-    const baseUrl = process.env.VITE_REACT_APP_API_URL || 'https://localhost';
+    const baseUrl = process.env.VITE_REACT_APP_API_URL || 'https://localhost:3000';
     const email = process.env.VITE_REACT_APP_TEST_EMAIL
     const password = process.env.VITE_REACT_APP_TEST_PASSWORD; 
 
