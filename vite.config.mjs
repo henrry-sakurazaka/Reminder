fa6ddabcddb1e232739de225b85ee0d0593f54a0
henrry-sakurazaka <bright.lights.21@me.com>
2024-11-28 12:06:34 +0900

    修正

--- vite.config.mjs
+++ vite.config.mjs
@@ -82,8 +82,8 @@ export default defineConfig({
     // 本番環境と開発環境で HTTPS の設定を分ける
     https: process.env.NODE_ENV === 'production' 
       ? {
-          key: fs.readFileSync(path.resolve(__dirname, './server.key.pem')), // 本番用証明書
-          cert: fs.readFileSync(path.resolve(__dirname, './server.cert.pem')), // 本番用証明書
+          key: fs.readFileSync(path.resolve(__dirname, '/app/server.key.pem')), // 本番用証明書
+          cert: fs.readFileSync(path.resolve(__dirname, '/app/server.cert.pem')), // 本番用証明書
         }
       : false, // 開発環境では HTTPS を無効化
 
