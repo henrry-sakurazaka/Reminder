34456483b6e34086387b420eb24e7a4475e61db9
henrry-sakurazaka <bright.lights.21@me.com>
2024-12-04 13:24:40 +0900

    修正

--- tests/3_edit.spec.js
+++ tests/3_edit.spec.js
@@ -17,7 +17,7 @@ const { test, expect } = require('@playwright/test');
   test.setTimeout(1200000);
 
   test('タスクをフォームを入力して出力を確認', async ({ page }) => {
-      const baseUrl =process.env.VITE_REACT_APP_API_URL || 'https://app:3000';
+      const baseUrl =process.env.VITE_REACT_APP_API_URL || 'https://localhost:3000';
       const email = process.env.VITE_REACT_APP_TEST_EMAIL
       const password = process.env.VITE_REACT_APP_TEST_PASSWORD; 
       
