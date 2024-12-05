df924ff0103ce7baee455b96cca20bdba0c7c215
henrry-sakurazaka <bright.lights.21@me.com>
2024-12-06 01:25:14 +0900

    test-resultsを削除して再生成

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -15,34 +15,34 @@ if (process.env.CI !== 'true') {
 
 export default defineConfig({
   testDir: './tests',  // テストファイルのディレクトリ
-  // timeout: 30000,  // テストのタイムアウト時間
-  // retries: 1,  // テストのリトライ回数
+  timeout: 30000,  // テストのタイムアウト時間
+  retries: 1,  // テストのリトライ回数
   // reporter: [],
-  // reporter: [
-  //   ['json', { outputFile: 'playwright-report/output.json' }],
-  //   ['html', { outputFolder: 'playwright-report', open: 'never' }],
-  //   ['list'],
-  //   ['html', { outputFolder: '/Users/Tsp33786/Desktop/trial_html/MY_WEB_SIGHT/reminder/test-results' }],
-  // ],
+  reporter: [
+    ['json', { outputFile: 'playwright-report/output.json' }],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['list'],
+    ['html', { outputFolder: '/Users/Tsp33786/Desktop/trial_html/MY_WEB_SIGHT/reminder/test-results' }],
+  ],
   // 動画キャプチャを無効にする
-  webServer: {
-    command: 'npm run dev',
-    url: process.env. VITE_REACT_APP_API_URL || 'https://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-  },
-  reporter: 'list',
+  // webServer: {
+  //   command: 'npm run dev',
+  //   url: process.env. VITE_REACT_APP_API_URL || 'https://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+  // reporter: 'list',
   use: {
     headless: true,  // ヘッドレスモードで実行（表示なし）
     viewport: { width: 1280, height: 720 },  // ビューポートの設定
     actionTimeout: 10000,  // アクションごとのタイムアウト
     baseURL: process.env.VITE_REACT_APP_API_URL || 'https://localhost:3000',
     ignoreHTTPSErrors: true,  // HTTPSエラーを無視
-    trace: 'on-first-retry',
-    video: 'off',  
-    // video: 'retain-on-failure',  // テスト失敗時にビデオ記録を保持
-    screenshot: 'off',
+    // trace: 'on-first-retry',
+    // video: 'off',  
+    video: 'retain-on-failure',  // テスト失敗時にビデオ記録を保持
+    // screenshot: 'off',
     // 証明書の設定を追加
-    outputDir: './custom-test-results', // 保存先を変更
+    // outputDir: './custom-test-results', // 保存先を変更
     launchOptions: {
       args: [
         '--ignore-certificate-errors', // 証明書エラーを無視
