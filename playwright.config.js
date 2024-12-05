ce6b97b9be991b9d263dbfe8808f2e795094e191
henrry-sakurazaka <bright.lights.21@me.com>
2024-12-05 15:53:17 +0900

    playwright.configを修正

--- playwright.config.js
+++ playwright.config.js
@@ -17,21 +17,23 @@ export default defineConfig({
   testDir: './tests',  // テストファイルのディレクトリ
   timeout: 30000,  // テストのタイムアウト時間
   retries: 1,  // テストのリトライ回数
-  reporter: [
-    ['json', { outputFile: 'playwright-report/output.json' }],
-    ['html', { outputFolder: 'playwright-report', open: 'never' }],
-    ['list'],
-    ['html', { outputFolder: '/Users/Tsp33786/Desktop/trial_html/MY_WEB_SIGHT/reminder/test-results' }],
-  ],
-  
+  reporter: [],
+  // reporter: [
+  //   ['json', { outputFile: 'playwright-report/output.json' }],
+  //   ['html', { outputFolder: 'playwright-report', open: 'never' }],
+  //   ['list'],
+  //   ['html', { outputFolder: '/Users/Tsp33786/Desktop/trial_html/MY_WEB_SIGHT/reminder/test-results' }],
+  // ],
+  // 動画キャプチャを無効にする
   use: {
     headless: true,  // ヘッドレスモードで実行（表示なし）
     viewport: { width: 1280, height: 720 },  // ビューポートの設定
     actionTimeout: 10000,  // アクションごとのタイムアウト
     baseURL: process.env.VITE_REACT_APP_API_URL || 'https://localhost:3000',
     ignoreHTTPSErrors: true,  // HTTPSエラーを無視
-    video: 'retain-on-failure',  // テスト失敗時にビデオ記録を保持
-
+    video: 'off',  
+    // video: 'retain-on-failure',  // テスト失敗時にビデオ記録を保持
+    screenshot: 'off',
     // 証明書の設定を追加
     launchOptions: {
       args: [
