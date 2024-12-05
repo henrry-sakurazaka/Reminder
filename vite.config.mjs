26c3ed3228f5579dcb8f95ad9aaac549d8bbdfd2
henrry-sakurazaka <bright.lights.21@me.com>
2024-12-05 23:27:06 +0900

    修正

--- vite.config.mjs
+++ vite.config.mjs
@@ -17,7 +17,6 @@ export default defineConfig({
     outDir: resolve(__dirname, 'dist'),
     define: {
       // 環境変数をコード内で使えるように設定
-      // 'process.env.VITE_FIREBASE_TOKEN': JSON.stringify(process.env.VITE_FIREBASE_TOKEN),
       'process.env.VITE_GOOGLE_APPLICATION_CREDENTIALS': JSON.stringify(process.env.VITE_GOOGLE_APPLICATION_CREDENTIALS),
     },
     emptyOutDir: true,
@@ -80,19 +79,18 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     // 本番環境と開発環境で HTTPS の設定を分ける
-    https: process.env.VITE_NODE_ENV === 'production' && process.env.CI === 'true'
-      // ? {
-      //     key: fs.readFileSync(path.resolve('/home/runner/Reminder/Reminder', 'server.key.pem')),
-      //     cert: fs.readFileSync(path.resolve('/home/runner/Reminder/Reminder', 'server.cert.pem'))
-      // }
-      // : {
-      //     key: fs.readFileSync('/etc/ssl/private/server.key.pem'),
-      //     cert: fs.readFileSync('/etc/ssl/certs/server.cert.pem'),
-      // },
+    https: process.env.VITE_NODE_ENV === 'production'
       ? {
             key: fs.readFileSync('/etc/ssl/private/server.key.pem'),
             cert: fs.readFileSync('/etc/ssl/certs/server.cert.pem'),
         } 
+      : undefined,
+
+    https: process.env.VITE_NODE_ENV === 'production' && process.env.CI === 'true'
+       ? {
+          key: fs.readFileSync(path.resolve('/home/runner/Reminder/Reminder', 'server.key.pem')),
+          cert: fs.readFileSync(path.resolve('/home/runner/Reminder/Reminder', 'server.cert.pem'))
+      }
       : undefined,
   
     hmr: true,
