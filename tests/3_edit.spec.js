ab19e2782036bb85b72c54b0fc8e47f58c146d89
henrry-sakurazaka <bright.lights.21@me.com>
2024-12-07 15:01:54 +0900

    修正

--- tests/3_edit.spec.js
+++ tests/3_edit.spec.js
@@ -21,15 +21,21 @@ import { test, expect } from '@playwright/test';
       
       await page.waitForTimeout(5000);   
       await page.goto(`${baseUrl}/SignIn`, {
-        waitUntil: 'load',
+        waitUntil: 'networkidle',
         timeout: 60000,
         ignoreHTTPSErrors: true, // これで証明書エラーを無視します
       });
-        
+      
+      await page.waitForTimeout(5000); // 必要に応じて時間を調整
+
       await page.waitForSelector('#email', { timeout: 30000 });
       await page.fill('#email', email, { timeout: 30000 }); 
+
+      await page.waitForTimeout(5000); // 必要に応じて時間を調整
+
       await page.waitForSelector('#password', { timeout: 30000 });
       await page.fill('#password', password, { timeout: 30000 }); 
+      
       await page.click('button.form-button[type="submit"]');
     
       await expect(page).toHaveURL(`${baseUrl}/Example`);
