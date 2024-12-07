667dbd37edc47a9ff09a1c0b0276671291dabf98
henrry-sakurazaka <bright.lights.21@me.com>
2024-12-07 14:09:29 +0900

    修正

--- tests/2_modal.spec.js
+++ tests/2_modal.spec.js
@@ -24,8 +24,10 @@ import { test, expect } from '@playwright/test';
       ignoreHTTPSErrors: true, // これで証明書エラーを無視します
     }); 
     
-    await page.fill('#email', email); 
-    await page.fill('#password', password); 
+    await page.waitForSelector('#email', { timeout: 15000});
+    await page.fill('#email', email, { timeout: 20000 }); 
+    await page.waitForSelector('#password', { timeout: 15000});
+    await page.fill('#password', password, { timeout: 20000}); 
     await page.click('button.form-button[type="submit"]');
   
     await expect(page).toHaveURL(`${baseUrl}/Example`);
