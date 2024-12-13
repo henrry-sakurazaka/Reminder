0052b3ad5a9c640b81affbbe2c24b04b72c26ad0
henrry-sakurazaka <bright.lights.21@me.com>
2024-12-14 00:20:19 +0900

    いろいろと変更

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -23,23 +23,14 @@ export default defineConfig({
     ['list'],
     ['html', { outputFolder: '/Users/Tsp33786/Desktop/trial_html/MY_WEB_SIGHT/reminder/test-results' }],
   ],
-  // //動画キャプチャを無効にする
-  // webServer: {
-  //   command: 'npm run dev',
-  //   url: process.env. VITE_REACT_APP_API_URL || 'https://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
-  // reporter: 'list',
+ 
   use: {
     headless: true,  // ヘッドレスモードで実行（表示なし）
     viewport: { width: 1280, height: 720 },  // ビューポートの設定
     actionTimeout: 10000,  // アクションごとのタイムアウト
     baseURL: process.env.VITE_REACT_APP_API_URL || 'https://localhost:3000',
     ignoreHTTPSErrors: true,  // HTTPSエラーを無視
-    // trace: 'on-first-retry',
-    // video: 'off',  
     video: 'retain-on-failure',  // テスト失敗時にビデオ記録を保持
-    // screenshot: 'off',
     //証明書の設定を追加
     // outputDir: './custom-test-results', // 保存先を変更
     launchOptions: {
