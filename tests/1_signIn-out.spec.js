11a0eac68f9aa99fb8249a6df2887d4c174d8aa6
henrry-sakurazaka <bright.lights.21@me.com>
2024-12-19 21:59:07 +0900

    ngrokを導入してhttpをhttpsにトンネル

--- tests/1_signIn-out.spec.js
+++ tests/1_signIn-out.spec.js
@@ -8,7 +8,7 @@ import { test, expect } from '@playwright/test';
 // });
 
   test('ログインテスト', async ({ page }) => {
-    const baseUrl = process.env.VITE_REACT_APP_API_URL || 'https://localhost:3000';
+    const baseUrl = process.env.VITE_REACT_APP_API_URL || 'http://localhost:3000';
     const email = process.env.VITE_REACT_APP_TEST_EMAIL
     const password = process.env.VITE_REACT_APP_TEST_PASSWORD; 
     
