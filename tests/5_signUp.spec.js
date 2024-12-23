eaad1abb3ec83949188cfd54af885c4ccc409d00
henrry-sakurazaka <bright.lights.21@me.com>
2024-12-23 10:47:16 +0900

    修正

--- tests/5_signUp.spec.js
+++ tests/5_signUp.spec.js
@@ -8,7 +8,7 @@ import { test, expect } from '@playwright/test';
 // }
 
   test('サインアップ機能のテスト', async ({ page }) => {
-    const baseUrl = process.env.VITE_REACT_APP_API_URL || 'http://localhost:3000';
+    const baseUrl = process.env.NGROK_URL || 'http://localhost:3000';
     const email = process.env.VITE_REACT_APP_TEST_EMAIL
     const password = process.env.VITE_REACT_APP_TEST_PASSWORD; 
     
