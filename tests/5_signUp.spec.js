3a90daeb39e52ec9def322f51170dbacde372997
henrry-sakurazaka <bright.lights.21@me.com>
2024-12-26 10:20:30 +0900

    修正

--- tests/5_signUp.spec.js
+++ tests/5_signUp.spec.js
@@ -1,11 +1,9 @@
 import 'dotenv/config';
-
 import { test, expect } from '@playwright/test';
 
-// // 環境がローカルであれば.envを読み込む
-// if (process.env.CI !== 'true') {
-//   require('dotenv').config();
-// }
+if (process.env.CI !== 'true') {
+  dotenv.config();
+}
 
   test('サインアップ機能のテスト', async ({ page }) => {
     const baseUrl = process.env.NGROK_URL;
