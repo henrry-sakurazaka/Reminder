828c366f9149f531ec40755ef1cd45b2ac56d0e2
henrry-sakurazaka <bright.lights.21@me.com>
2025-01-01 20:00:25 +0900

    修正

--- tests/1_signIn-out.spec.js
+++ tests/1_signIn-out.spec.js
@@ -11,7 +11,7 @@ if (process.env.CI !== 'true') {
 // });
 
   test('ログインテスト', async ({ page }) => {
-    const baseUrl = process.env.VITE_REACT_APP_API_URL || "http://localhost:3000";
+    const baseUrl = process.env.VITE_REACT_APP_API_URL || "http://app2:3000";
     const email = process.env.VITE_REACT_APP_TEST_EMAIL
     const password = process.env.VITE_REACT_APP_TEST_PASSWORD; 
 
