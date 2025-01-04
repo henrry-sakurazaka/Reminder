0e55fa1ec08396816a96d64521cf0d4f09a29d08
henrry-sakurazaka <bright.lights.21@me.com>
2025-01-04 21:36:11 +0900

    test-network2を作成。

--- tests/3_edit.spec.js
+++ tests/3_edit.spec.js
@@ -17,12 +17,13 @@ if (process.env.CI !== 'true') {
     const baseUrl = process.env.VITE_REACT_APP_API_URL || "http:/app2:3000";
     const email = process.env.VITE_REACT_APP_TEST_EMAIL
     const password = process.env.VITE_REACT_APP_TEST_PASSWORD; 
-    
+
       async ({ browser }) => {
         const context = await browser.newContext(); // 新しいコンテキストを生成
         const page = await context.newPage();
         await page.goto(`${baseUrl}/UserAuth`);
         await context.clearCookies();
+        await context.clearPermissions();
         await page.evaluate(() => {
             localStorage.clear();
             sessionStorage.clear();     
