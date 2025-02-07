9034c6224de55e3b479c486eb55807c460dec94b
henrry-sakurazaka <bright.lights.21@me.com>
2025-02-07 21:13:48 +0900

    retry

--- .cloudflared/entrypoint.sh
+++ .cloudflared/entrypoint.sh
@@ -2,11 +2,11 @@
 
 # 1. Named Tunnel を実行（TUNNEL_IDが必要）
 if [ -n "${TUNNEL_ID}" ]; then
-  exec cloudflared tunnel --no-autoupdate run "${TUNNEL_ID}"
+  exec cloudflared tunnel --no-autoupdate run "${TUNNEL_ID}" &
 elif [ -f "/home/runner/.cloudflared/offsetcodecraft.site.json" ]; then
-  exec cloudflared tunnel --credentials-file /home/runner/.cloudflared/offsetcodecraft.site.json run offsetcodecraft.site
+  exec cloudflared tunnel --credentials-file /home/runner/.cloudflared/offsetcodecraft.site.json run offsetcodecraft.site &
 elif [ -f "/home/runner/.cloudflared/config.yml" ]; then
-  exec cloudflared tunnel --config /home/runner/.cloudflared/config.yml run
+  exec cloudflared tunnel --config /home/runner/.cloudflared/config.yml run &
 else
   echo "Error: No valid tunnel configuration found."
   exit 1
