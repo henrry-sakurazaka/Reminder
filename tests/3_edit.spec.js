cd9bc8041d0087fe0151f5a315d51f9634623dbd
henrry-sakurazaka <bright.lights.21@me.com>
2025-02-15 09:54:25 +0900

    retry

--- tests/3_edit.spec.js
+++ tests/3_edit.spec.js
@@ -35,23 +35,22 @@ if (process.env.CI !== 'true') {
         timeout: 60000,
         ignoreHTTPSErrors: true, // これで証明書エラーを無視します
       }); 
+
       await page.click('span#SI', { timeout: 30000 });
       await page.waitForTimeout(5000); 
       await expect(page).toHaveURL(`${baseUrl}/SignIn`);
+
       await page.waitForTimeout(5000); 
 
       await page.waitForSelector('#email', { timeout: 30000 });
       await page.fill('#email', email, { timeout: 30000 }); 
+      await page.fill('#password', password); 
+      await page.click('button.form-button[type="submit"]');
 
       await page.waitForTimeout(5000); 
 
-      await page.waitForSelector('#password', { timeout: 30000 });
-      await page.fill('#password', password, { timeout: 30000 }); 
-      
-      await page.click('button.form-button[type="submit"]');
-    
       await expect(page).toHaveURL(`${baseUrl}/Example`);
-        
+
       await page.fill('input#task','test');
       await page.click('button.add');
       const lastSpan = page.locator('span.content').last(); 
