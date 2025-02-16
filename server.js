a848fbe7d79e462fbc655dc6a2478bf848651fba
henrry-sakurazaka <bright.lights.21@me.com>
2025-02-16 18:35:09 +0900

    server.jsにcorsミドルウェアを設定

--- server.js
+++ server.js
@@ -260,6 +260,7 @@
 // });
 
 import express from 'express';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
@@ -267,6 +268,10 @@ const app = express();
 
 // ミドルウェア
 app.use(express.json());
+// CORSミドルウェアを使用
+app.use(cors({
+  origin: 'https://offsetcodecraft.site'
+}));
 
 // ルート
 app.get('/', (req, res) => {
