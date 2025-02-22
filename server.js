8679eccb7b8b8198cb7501b10af645fcdf18fd40
henrry-sakurazaka <bright.lights.21@me.com>
2025-02-22 20:13:27 +0900

    修正

--- server.js
+++ server.js
@@ -266,22 +266,14 @@ const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || '0.0.0.0';
 const app = express();
 
-const corsOptions = {
-  origin: [
-      'https://reminder3-65e84.web.app', 
-      'http://localhost:3000', 
-      'https://offsetcodecraft.site'
-    ],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
-
 // const corsOptions = {
-//   origin: '*', // すべてのオリジンを許可
+//   origin: [
+//       'https://reminder3-65e84.web.app', 
+//       'http://localhost:3000', 
+//       'https://offsetcodecraft.site'
+//     ],
 //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-//   allowedHeaders: '*', // すべてのヘッダーを許可
+//   allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
 //   credentials: true,
 //   optionsSuccessStatus: 204,
 // };
@@ -291,7 +283,8 @@ dotenv.config();
 // ミドルウェア
 app.use(express.json());
 // CORSミドルウェアを使用
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 
 // ルート
 app.get('/', (req, res) => {
