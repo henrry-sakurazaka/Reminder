d14c258616454ffdcd4312fcd1465102ac6e81de
henrry-sakurazaka <bright.lights.21@me.com>
2025-02-25 19:57:36 +0900

    修正

--- vite.config.mjs
+++ vite.config.mjs
@@ -68,13 +68,14 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
-    allowedHosts: [
-      'app2', 
-      'localhost', 
-      '0.0.0.0', 
-      'offsetcodecraft.site',
-      '172.18.0.4',
-    ],
+    // allowedHosts: [
+    //   'app2', 
+    //   'localhost', 
+    //   '0.0.0.0', 
+    //   'offsetcodecraft.site',
+    //   '172.18.0.4',
+    // ],
+    allowedHosts: ['app2'],
     // https:
     //   process.env.VITE_NODE_ENV === 'production' || process.env.VITE_HTTPS === 'true'
     //     ? {
