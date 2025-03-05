2376959562977290ad126a66be7f10ff438846d5
henrry-sakurazaka <bright.lights.21@me.com>
2025-03-05 11:30:27 +0900

    修正

--- functions/index.js
+++ functions/index.js
@@ -51,7 +51,8 @@ admin.initializeApp({
 
 const app = express();
 const isEmulator = process.env.FUNCTIONS_EMULATOR === "true";
-const PORT = isEmulator ? 6000 : process.env.PORT || 6080; 
+const PORT = isEmulator ? 6000 : 6080;
+// const PORT = isEmulator ? 6000 : process.env.PORT || 6080;
 
 
 // CORSのミドルウェアを設定
