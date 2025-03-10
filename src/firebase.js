46b513d10e69c59d965ed70438174aa7d88492a9
henrry-sakurazaka <bright.lights.21@me.com>
2025-03-10 17:34:34 +0900

    修正

--- src/firebase.js
+++ src/firebase.js
@@ -18,15 +18,15 @@ const firebaseConfig = {
   databaseURL: import.meta.env.VITE_REACT_APP_FIREBASE_DATABASE_URL,
 };
 
-// Firebase Admin SDKの初期化
-const serviceAccount = require('./serviceAccountKey.json'); // サービスアカウントの認証情報ファイルのパス
+// // Firebase Admin SDKの初期化
+// const serviceAccount = require('./serviceAccountKey.json'); // サービスアカウントの認証情報ファイルのパス
 
-if (getApps().length === 0) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: import.meta.env.VITE_REACT_APP_FIREBASE_DATABASE_URL,
-  });
-}
+// if (getApps().length === 0) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: import.meta.env.VITE_REACT_APP_FIREBASE_DATABASE_URL,
+//   });
+// }
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 // const app = initializeApp(firebaseConfig);
