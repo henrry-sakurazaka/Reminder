df53f2bbcf69a2bc1426e60b6a48541611263e00
henrry-sakurazaka <bright.lights.21@me.com>
2025-03-11 07:03:59 +0900

    修正

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -127,9 +127,12 @@ function SignUp() {
         );
         const user = userCredential.user;
 
-        await updateProfile(auth.currentUser, {
-          displayName: name,
-        });
+        await updateProfile(user,{displayName: name,});
+
+        // await updateProfile(auth.currentUser, {
+        //   displayName: name,
+        // });
+        
         const convertedData = todosConverter2.toFirestore(todoList);
         const dataWithUid = {
           todoId: user.uid,
