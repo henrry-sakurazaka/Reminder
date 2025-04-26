611956644932d2da7825ee70e9a1935fc7d8b5d0
henrry-sakurazaka <bright.lights.21@me.com>
2025-04-26 21:29:11 +0900

    Update dist

--- dist/dist/assets/index-Dvm9c_Tr.js
+++ dist/dist/assets/index-Dvm9c_Tr.js
@@ -37,9 +37,9 @@ import { getDatabase as u } from 'https://www.gstatic.com/firebasejs/10.12.2/fir
 })();
 const p = {
     apiKey: 'AIzaSyCFn-eJuAP2f2zYP4VxMvvwef15jzyW7bA',
-    authDomain: 'reminder5-27ef0.firebaseapp.com',
-    projectId: 'reminder5-27ef0',
-    storageBucket: 'reminder5-27ef0.appspot.com',
+    authDomain: 'reminder3-65e84.firebaseapp.com',
+    projectId: 'reminder3-65e84',
+    storageBucket: 'reminder3-65e84.appspot.com',
     messagingSenderId: '280162142902',
     appId: '1:280162142902:web:4fed1bc9d4b35e75963417',
     measurementId: 'G-C0NL3GWNWZ',
