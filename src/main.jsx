2c338328d49d31b43d4bcf1be1692a84f7db742e
henrry-sakurazaka <bright.lights.21@me.com>
2025-05-01 14:06:43 +0900

    Resolve path firebase/auth

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,6 @@ import reportWebVitals from './reportWebVitals';
 
 const root = createRoot(document.getElementById('root')); // root DOM要素の取得
 
-if (typeof window !== 'undefined') {
-  console.log('🔥 クライアントで実行中');
-  console.log(
-    '🔑 Firebase config:',
-    import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY
-  );
-} else {
-  console.log('❄️ サーバーサイドで実行中');
-}
-
 root.render(
   <React.StrictMode>
     <App />
